930a66fc3e8bb4844e414b4441feff1ad3a4a243
Hokila <hokilajan@kkbox.com>
2015-08-23 01:57:42 +0800

    Add BusDriverView.

---
+++ BeaBus/BeaBus/BusDriverViewController.m
@@ -0,0 +1,41 @@
+//
+//  BusDriverViewController.m
+//  BeaBus
+//
+//  Created by 福氣啦 \囧/ on 2015/8/23.
+//  Copyright (c) 2015年 Wayne. All rights reserved.
+//
+
+#import "BusDriverViewController.h"
+
+@interface BusDriverViewController ()
+
+@end
+
+@implementation BusDriverViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(BOOL)prefersStatusBarHidden{
+    return YES;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
