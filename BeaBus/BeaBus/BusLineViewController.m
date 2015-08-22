94cc14ca65d78061665b50bd026c947cd3b68a65
Peni <smallr@gmail.com>
2015-08-22 04:53:04 -0700

    Add collection view to show bus stop info.

--- BeaBus/BeaBus/BusLineViewController.m
+++ BeaBus/BeaBus/BusLineViewController.m
@@ -8,7 +8,44 @@
 
 #import "BusLineViewController.h"
 
+@interface BusStopInfo : NSObject
+
+@property (strong, nonatomic) NSString *busStopName;
+@property (strong, nonatomic) NSString *driverName;
+@property (strong, nonatomic) NSString *licenseNumber;
+@property (strong, nonatomic) NSString *remainingTime;
+
+@end
+
+@implementation BusStopInfo
+
+- (id)initWithBusStopName:(NSString *)busStopName driverName:(NSString *)driverName licenseNumber:(NSString *)licenseNumber remainingTime:(NSString *)remainingTime
+{
+    self = [self init];
+    
+    self.busStopName = busStopName;
+    self.driverName = driverName;
+    self.licenseNumber = licenseNumber;
+    self.remainingTime = remainingTime;
+    
+    return self;
+}
+
+@end
+
+
+@interface BusStopCell : UICollectionViewCell
+
+@end
+
+@implementation BusStopCell
+
+@end
+
+
 @interface BusLineViewController ()
+
+@property (strong, nonatomic) NSMutableArray *busStopInfos;
 
 @end
 
@@ -17,6 +54,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.busStopInfos = [NSMutableArray new];
+    [self.busStopInfos addObject:[[BusStopInfo alloc] initWithBusStopName:@"調度站錦繡站" driverName:@"林家豪" licenseNumber:@"356-FM" remainingTime:@"3分"]];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -28,8 +68,29 @@
     return YES;
 }
 
+#pragma mark - Action
+
 - (IBAction)nextButtonAction:(id)sender {
     [self performSegueWithIdentifier:@"goToRankingView" sender:self];
+}
+
+#pragma mark - UICollectionView data source
+
+-(NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+    return 1;
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:      (NSInteger)section
+{
+    return 10;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    BusStopCell *cell = (BusStopCell *)[collectionView dequeueReusableCellWithReuseIdentifier:@"BusStopCell" forIndexPath:indexPath];
+    
+    return cell;
 }
 
 /*
